a97429a30cb3e422a284676f24e6bbfd76349501
LYang0405 <893066499@qq.com>
2020-12-21 17:48:47 +0800

    2 times

---
+++ GoNuts/CoffeePage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace GoNuts
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class CoffeePage : Page
+    {
+        public CoffeePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private string _roast;
+        private string _sweetener;
+        private string _cream;
+
+        private void displayResult()
+        {
+            if(_roast == "None" || String.IsNullOrEmpty(_roast))
+            {
+                ResultTextBlock.Text = "None";
+            }
+            ResultTextBlock.Text = _roast;
+            if(_cream != "None" || !String.IsNullOrEmpty(_cream))
+            {
+                ResultTextBlock.Text += " +" + _cream;
+            }
+            if(_sweetener != "None" || !String.IsNullOrEmpty(_sweetener))
+            {
+                ResultTextBlock.Text += "+" + _sweetener;
+            }
+        }
+
+        private void Roast_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _roast = selected.Text;
+            displayResult();
+        }
+
+        private void Sweetener_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _sweetener = selected.Text;
+            displayResult();
+        }
+
+        private void Cream_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _cream = selected.Text;
+            displayResult();
+        }
+    }
+}
